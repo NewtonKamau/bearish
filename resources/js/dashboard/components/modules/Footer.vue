1cd2b333a13d69ce62d36480474218136a55eb05
NewtonKamau <kamaunewton78@gmail.com>
2021-12-12 11:04:38 +0300

    initial commit

---
+++ resources/js/dashboard/components/modules/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+    <footer class="content-footer">
+        <div class="footer">
+            <div class="copyright">
+                <span class="go-right mb-3">
+                    {{ $t("Created with") }}
+                    <i class="fas fa-heart" style="color: #ff4c4c" />
+                    {{ $t("by") }}
+                    <a
+                        href="https://www.goldinsacs.com/"
+                        rel="nofollow"
+                        target="_blank"
+                        >goldinsacs</a
+                    >
+                    | {{ $t("Powered by") }}
+                    <i class="fab fa-laravel" style="color: #f55247" /> Laravel
+                </span>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: "Footer",
+};
+</script>
